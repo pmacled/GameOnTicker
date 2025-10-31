4571d263d044465391d01f3b6c29c16ba4bcd305
pmacled <nash.delcamp@gmail.com>
2025-10-31 11:26:24 -0400

    disconnect database connection

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,9 @@
 #' @noRd
 app_server <- function(input, output, session) {
   db_conn <- gameon_db_connect()
+  session$onSessionEnded(function() {
+    DBI::dbDisconnect(db_conn)
+  })
 
   mod_referee_controls_server("referee_controls_1", db_conn, game_id = 170)
   mod_play_by_play_view_server("play_by_play_view_1", db_conn, game_id = 170)
